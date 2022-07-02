621dd8049e9bb69497757145ff4a7a69bc850784
nguyenvandungb <dungnv20384@gmail.com>
2022-07-02 14:31:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng-zorro-layout-responsive-xnr7bv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng-zorro-layout-responsive-xnr7bv)
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_ICONS } from 'ng-zorro-antd';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+
+import { NzDemoLayoutResponsiveComponent } from './app.component';
+
+import { NZ_I18N, en_US } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+registerLocaleData(en);
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule, NgZorroAntdModule, BrowserAnimationsModule ],
+  declarations: [ NzDemoLayoutResponsiveComponent ],
+  bootstrap:    [ NzDemoLayoutResponsiveComponent ],
+  providers   : [ { provide: NZ_I18N, useValue: en_US }, { provide: NZ_ICONS, useValue: icons } ]
+})
+export class AppModule { }
